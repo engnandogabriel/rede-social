66822ec6fe6e6d9806da5f4aa91153e1e45fe2ef
engnandogabriel <eng.nandogabriel@gmail.com>
2023-07-26 09:41:10 -0300

    feat: feed

---
+++ src/componets/Photo/PhotoCommentes/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { PhotoCommentStyled } from "../../../styles/Photo";
+
+const PhotoCommentes = ({ id, comments }) => {
+  return <PhotoCommentStyled>Comentários</PhotoCommentStyled>;
+};
+
+export default PhotoCommentes;
